2d59514ee2b4bce9d034fe7761c9f25a426f0fde
isyouyoung <isyouyoung@gmail.com>
2025-06-20 10:11:32 +0900

    25-06-20-01 C_study fix

--- 25-06-20-01.c
+++ 25-06-20-01.c
@@ -2,13 +2,14 @@
 
 void main()
 {
-	int i,j,cnt;
-	int hap = 0;
+	int i,j;
+	int hap=0;
 
 	for (i=1; i<=4; i++){
-	printf("%d번째 숫자를 입력하세요 : ",i);
-	scanf("%d\n",&cnt);
-	hap += i;
+		printf("%d번째 숫자를 입력하세요 : ",i);
+		scanf("%d",&j);
+		hap += j;
 	}	
+	printf(" 합계 ==> %d",hap);
 }
 
