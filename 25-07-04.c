7829a2969d119038205e8357757a9bfb7dd62de6
isyouyoung <isyouyoung@gmail.com>
2025-07-04 16:00:25 +0900

    25-07-04 C_study 3.push

--- 25-07-04.c
+++ 25-07-04.c
@@ -13,4 +13,11 @@ void main()
 	}
 
 	printf("문자열 배열 ss => %s \n",ss);
+	printf("\n");
+	printf("크기가 8인 문자형 배열을 선언하고 초기화함\n");
+	printf("여섯 번째 문자를 바꿈\n");
+	printf("여덞 번 반복하면서 배열 ss의 각 문자를 출력함\n");
+	printf("배열 ss의 전체 문자열을 출력함\n");
+
 }
+
