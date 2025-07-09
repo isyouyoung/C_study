449ac06d41d5635b946a829ba7129b88b1db828e
isyouyoung <isyouyoung@gmail.com>
2025-07-09 18:59:36 +0900

    25-07-09 C_study code finsh ver0.02

--- 25-07-09.c
+++ 25-07-09.c
@@ -2,6 +2,16 @@
 
 void main()
 {
-	printf("거꾸로 출력한 결과==>");
+	char ss[5] = "abcd";
+	char tt[5];
+	int i;
+
+	for(i=0 ; i<4 ; i++)
+	{
+		tt[i] = ss[3-i] ;
+	}
+	tt[4] = '\0';
+
+	printf("거꾸로 출력한 결과==> %s \n", tt);
 }
 
