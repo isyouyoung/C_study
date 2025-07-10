14f07f74d53667cd2ac4303ed12dd111c4fa22a4
isyouyoung <isyouyoung@gmail.com>
2025-07-10 10:36:02 +0900

    last update

--- 25-07-10.c
+++ 25-07-10.c
@@ -9,5 +9,7 @@ void main()
 	len = strlen(ss);
 
 	printf("문자열 \"%s\"의 길이 ==> %d \n", ss, len);
-	printf("\n 큰따옴표의 내용을 출력하기 위해 \"문자를 사용함");
+	printf("\n 큰따옴표의 내용을 출력하기 위해 \"문자를 사용함\n");
+
+	printf("\n공부중=>코드 분석중");
 }
