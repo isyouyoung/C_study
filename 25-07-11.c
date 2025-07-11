fa6ba397304882362cf85a6a92e273e98004c1ed
isyouyoung <isyouyoung@gmail.com>
2025-07-11 10:23:46 +0900

    25-07-11 C_study 8-10 code ver0.03

--- 25-07-11.c
+++ 25-07-11.c
@@ -8,5 +8,8 @@ void main()
 	strcpy(ss, "XYZ");
 
 	printf("문자열 ss의 내용 ==> %s \n", ss);
+
+	printf("\nss[4]로 선언한 배열에 srycpy를 이용하여 XYZ를 넣어주었음\n");
+	printf("중요한점은 ss[4]라고 4칸을 넣어선안된다는것 마지막은 널칸이다\n");
 }
 
